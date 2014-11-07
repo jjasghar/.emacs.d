84754cd24b3ebac7fc89307c914b70b6757fde85
JJ Asghar <jj@getchef.com>
2014-11-07 16:10:30 +0100

    Added org-present things

--- my-orgmode.el
+++ my-orgmode.el
@@ -4,3 +4,16 @@
 (define-key global-map "\C-ct" 'org-capture)
 (setq org-export-backends (quote (ascii html icalendar latex md)))
 (setq org-startup-indented t)
+
+(add-hook 'org-present-mode-hook
+          (lambda ()
+            (org-present-big)
+            (org-display-inline-images)))
+
+(add-hook 'org-present-mode-quit-hook
+          (lambda ()
+            (org-present-small)
+            (org-remove-inline-images)))
+
+;; fontify code in code blocks
+(setq org-src-fontify-natively t)
