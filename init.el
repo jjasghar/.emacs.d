0dfed133c5dcd846a4d484bb711fcb1f15d93208
JJ Asghar <jj@chef.io>
2017-01-04 13:28:18 -0600

    added a few things

--- init.el
+++ init.el
@@ -27,13 +27,13 @@
    (quote
     ((:name "inbox" :query "tag:inbox" :key "i" :sort-order newest-first :search-type nil)
      (:name "all mail" :query "*" :key "a" :sort-order newest-first)
-     (:name "today" :query "date:midnight..now" :sort-order newest-first))))
+     (:name "today" :query "date:midnight..now" :sort-order newest-first))) t)
  '(org-agenda-files
    (quote
     ("/Users/jasghar/org/notes.org" "/Users/jasghar/org/blah_priorites_date_.org" "/Users/jasghar/org/caching_project.org" "/Users/jasghar/org/docker.org" "/Users/jasghar/org/emacs.org" "/Users/jasghar/org/freenode.org" "/Users/jasghar/org/gem.org" "/Users/jasghar/org/guenter.org" "/Users/jasghar/org/hanlon.org" "/Users/jasghar/org/index.org" "/Users/jasghar/org/ipxe.org" "/Users/jasghar/org/irc.org" "/Users/jasghar/org/kitchen-cluster.org" "/Users/jasghar/org/landing_page_meeting.org" "/Users/jasghar/org/maas.org" "/Users/jasghar/org/magit.org" "/Users/jasghar/org/notes.nov3014.org" "/Users/jasghar/org/openbay_openstack.org" "/Users/jasghar/org/openstack_documentation.org" "/Users/jasghar/org/openstack_priorities.20150108.org" "/Users/jasghar/org/pry.org" "/Users/jasghar/org/rfc-for-upstart.org" "/Users/jasghar/org/singlestack.org" "/Users/jasghar/org/software_licences.org" "/Users/jasghar/org/statusmeetings.org" "/Users/jasghar/org/test.org" "/Users/jasghar/org/test_bable.org")))
  '(package-selected-packages
    (quote
-    (avy-zap avy writegood-mode google-this ivy true swiper toml-mode org htmlize ox-reveal ox-twbs yaml-mode notmuch web-mode git-gutter zenburn-theme gist markdown-mode org-bullets yasnippet try solarized-theme magit-gh-pulls magit-gerrit helm guide-key flycheck company)))
+    (golden-ratio go-mode twittering-mode terraform-mode ag flycheck-rust rust-mode kanban avy-zap avy writegood-mode google-this ivy true swiper toml-mode org htmlize ox-reveal ox-twbs yaml-mode notmuch web-mode git-gutter zenburn-theme gist markdown-mode org-bullets yasnippet try solarized-theme magit-gh-pulls magit-gerrit helm guide-key flycheck company)))
  '(send-mail-function nil)
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 (custom-set-faces
@@ -42,3 +42,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(region ((t (:inherit nil :background "RoyalBlue4")))))
+(put 'narrow-to-region 'disabled nil)
