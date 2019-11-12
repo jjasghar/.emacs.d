ed225170a603644aa5c3a58685073c746fa1c2c5
JJ Asghar <jjasghar@gmail.com>
2019-11-12 16:01:12 -0600

    Added melpa, because well melpa.
    Signed-off-by: JJ Asghar <awesome@ibm.com>

--- init.el
+++ init.el
@@ -6,3 +6,21 @@
 (package-initialize)
 
 (org-babel-load-file "/home/jj/.emacs.d/my-init.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:added-sign "+")
+ '(git-gutter:deleted-sign "-")
+ '(package-selected-packages
+   (quote
+    (ox-twbs yasnippet-snippets yaml-mode writegood-mode web-mode use-package toml-mode terraform-mode swiper rust-mode restclient powershell org-re-reveal org-present markdown-mode kubernetes-tramp kubernetes js2-mode helm guide-key-tip groovy-mode go-mode git-link gist flycheck-rust ecukes dokuwiki-mode dockerfile-mode company avy atomic-chrome)))
+ '(send-mail-function nil)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:inherit nil :background "RoyalBlue4")))))
